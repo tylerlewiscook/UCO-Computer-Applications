8bd8442d60aa82da655a5ca9999fc9d0b1163088
tylerlewiscook <8811690+tylerlewiscook@users.noreply.github.com>
2020-01-27 13:27:29 -0600

    Add files via upload

---
+++ Code/03-RandomVariables.R
@@ -0,0 +1,31 @@
+# Random Variables
+
+# Probabilities --------------------------------------------
+dnorm(0)            # Density values: f(x) or p(x)
+
+pnorm(0)            # Distribution values: F(x)
+
+qnorm(0.5)          # Quantile values 
+
+
+
+# Random number generation --------------------------------
+set.seed(1)
+
+rnorm(10)           
+rnorm(10, 50, 5)
+
+
+
+# Examples from slides ------------------------------------
+dbinom(6, 19, 0.504)
+pbinom(6, 19, 0.504)
+
+dpois(10, 9)
+
+1 - pnorm(240, 170, 30)
+pnorm(240, 170, 30, lower.tail = FALSE)
+pnorm(180, 170, 30) - pnorm(140, 170, 30)
+qnorm(.9, 170, 30)
+qnorm(.1, 170, 30, lower.tail = FALSE)
+
