af7894b3697f5664046709a1136d666f150ea971
tylerlewiscook <8811690+tylerlewiscook@users.noreply.github.com>
2023-09-18 15:32:53 -0500

    Update 03-InferenceMeans.R

--- Code/03-InferenceMeans.R
+++ Code/03-InferenceMeans.R
@@ -25,27 +25,12 @@ pt(out2$statistic, 152)
 
 # Two sample ------------------------------------------------------------------------
 
-library(ISLR)
+library(ISLR2)
 data(College)
 
 # Independent
 t.test(Accept ~ Private, data = College)
 t.test(Accept ~ Private, var.equal = TRUE, data = College)
-
-aggregate(Accept ~ Private, data = College, mean)
-mean_public <- aggregate(Accept ~ Private, data = College, mean)[1,2]
-mean_private <- aggregate(Accept ~ Private, data = College, mean)[2,2]
-
-aggregate(Accept ~ Private, data = College, var)
-var_public <- aggregate(Accept ~ Private, data = College, var)[1,2]
-var_private <- aggregate(Accept ~ Private, data = College, var)[2,2]
-
-n_public <- as.numeric(table(College$Private)[1])
-n_private <- as.numeric(table(College$Private)[2])
-
-t <- (mean_public - mean_private)/sqrt(var_public/n_public + var_private/n_private)
-t
-
 
 # Paired
 before <- c(180, 195, 235, 250, 210, 300)
